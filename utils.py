799681e5e84664baf02daae83e50b704f013b3d0
Brian Timar <timarbrian@gmail.com>
2019-05-03 19:38:48 -0700

    import utils and notebook

---
+++ utils.py
@@ -0,0 +1,7 @@
+def get_photo_url(photo):
+    """Returns url string corresponding to the json photo object."""
+    return photo['url']
+
+def get_all_photo_urls(person):
+    """ Return list of all photos urls in json object person."""
+    return 
