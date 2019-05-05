a209bb4408ccef7aa855bebaec47adce6bd936fc
Brian Timar <timarbrian@gmail.com>
2019-05-04 21:34:21 -0700

    minimal common-face matching example

--- face_utils.py
+++ face_utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv2
 import os
 
+
 def get_align_and_net(imgDim=96):
     """ Returns dlib model for finding and aligning faces, and torch model 
     for computing representations."""
@@ -49,6 +50,10 @@ def annotate_image(image, bounding_boxes):
         bounding_boxes: an iterable holding dlib rectangles.
         returns: new image with each bounding box marked.
         """
+    try:
+        Nbox = len(bounding_boxes)
+    except TypeError:
+        bounding_boxes = [bounding_boxes]
     image = image.copy()
     for box in bounding_boxes:
         bl = (box.left(), box.bottom())
@@ -66,7 +71,7 @@ def get_all_aligned_faces(image, align, imgDim=96):
 
     bboxes = get_bounding_boxes(image, align)
     if len(bboxes) ==0:
-        raise ValueError("No faces found!")
+        return [], []
     aligned_faces = []
     for box in bboxes:
         alignedFace = align.align(imgDim, image, box,
@@ -87,6 +92,8 @@ def pull_boxes_and_reps(image, align, net, imgDim=96):
 
     newfaces = []
     aligned_faces, bboxes = get_all_aligned_faces(image, align, imgDim=imgDim)
+    if len(aligned_faces) ==0:
+        return []
     for i in range(len(aligned_faces)):
         newfaces.append(dict(box=bboxes[i],
                             rep=net.forward(aligned_faces[i])))
@@ -95,3 +102,56 @@ def pull_boxes_and_reps(image, align, net, imgDim=96):
 def dist(rep1, rep2):
     """ Returns the Euclidean distance between two face reps"""
     return np.sum((rep1 - rep2)**2)
+
+def locate_common_reps(reps_by_image, keep_all_singles=True):
+    """reps_by_image = a list of lists of reps, one for each image in user's profile. 
+        keep_all_singles: if True, assumes all single-face images contain user. 
+        Return a list of lists, each giving the index of the rep assigned to the user in a particular 
+        image. """
+    return kmeans_cluster_indices(reps_by_image, keep_all_singles=keep_all_singles)
+
+
+
+def kmeans_cluster_indices(reps_by_image, keep_all_singles=True):
+    """reps_by_image = a list of lists of reps, one for each image in user's profile. 
+        Return a list of lists, each giving the index of the rep assigned to the user in a particular 
+        image.
+        keep_all_singles: if True, assumes all single-face images contain user. 
+  """
+
+    from sklearn.cluster import KMeans
+    reps_pooled = []
+    for im in reps_by_image:
+        reps_pooled += im
+    reps = np.asarray(reps_pooled)
+    n_clusters=2
+    kmeans = KMeans(n_clusters=n_clusters).fit(reps)
+    pop_by_label = []
+    for i in range(n_clusters):
+        pop_by_label.append( np.sum(kmeans.labels_ == i))
+    if max(pop_by_label) > len(reps_by_image):
+        raise ValueError("Rep is duplicated in image!")    
+    #cluster label assigned to the user
+    user_label = pop_by_label.index(max(pop_by_label))
+
+    user_indices = []
+    for i in range(len(reps_by_image)):
+        im = reps_by_image[i]
+        #if empty, no face was found in that image, just move on
+        if len(im)==0:
+            user_index = None
+        elif len(im)==1 and keep_all_singles:
+            user_index = 0
+        else:
+            labels = kmeans.predict(im)
+            user_ims = labels==user_label
+            if sum(user_ims) > 1:
+                raise ValueError("Rep is duplicated in image")
+            else:
+                if sum(user_ims) == 0:
+                    user_index = None
+                else:
+                    user_index = np.arange(len(im), dtype=int)[user_ims][0]
+
+        user_indices.append(user_index)
+    return user_indices
